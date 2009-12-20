270f73b05a5041756448c9dbd02dff2f76a1fac5
tarcisio <sassara.tarcisio@gmail.com>
2009-12-20 03:48:49 -0200

    criado o arquivo contendo funcoes utilitarias

---
+++ utils.php
@@ -0,0 +1,34 @@
+<?php
+
+define('USERAGENT', 'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.5) Gecko/20091109 Ubuntu/9.10 (karmic) Firefox/3.5.5');
+
+function curl($url, $parametros)
+{
+    $par_serial = '';
+    foreach($parametros as $chave => $valor)
+    {
+        $par_serial .= sprintf('%s=%s&', $chave, $valor);
+    }
+    $par_serial = trim($par_serial, '&');
+    
+    $opcoes = array(
+        CURLOPT_URL        => $url,
+        CURLOPT_POST       => count($parametros),
+        CURLOPT_POSTFIELDS => $par_serial,
+        CURLOPT_USERAGENT  => USERAGENT,
+        CURLOPT_RETURNTRANSFER => true
+    );
+    /** Inicializa o curl */
+    $curl = curl_init();
+    
+    curl_setopt_array($curl, $opcoes);
+    
+    /** pega o conteudo */
+    $conteudo = curl_exec($curl);
+    $conteudo = iconv('ISO-8859-1', '"UTF-8//TRANSLIT"', $conteudo);
+    
+    /** fecha o curl */
+    curl_close($curl);
+    
+    return $conteudo;
+}
