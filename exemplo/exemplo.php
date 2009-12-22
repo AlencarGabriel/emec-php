5fa8193c313a6ab0dfaf7aa37cbe95c69caa60bf
tarcisio <sassara.tarcisio@gmail.com>
2009-12-22 02:41:23 -0200

    criado o exemplo

--- exemplo/exemplo.php
+++ exemplo/exemplo.php
@@ -1,34 +1,78 @@
 #!/usr/bin/env php
 <?php
-/*
-pegar o curso
-pegar as paginas dos cursos
-pegar as urls de cada pagina
 
-*/
+error_reporting(-1);
 
-require_once(dirname(__FILE__).'/exemplo_utils.php');
+$cursos = array('PSICOLOGIA');
 
-inicializa_diretorio('tmp');
-
-
-$cursos = array('MATEMATICA', 'ENFERMAGEM', 'PSICOLOGIA');
-
+// Para cada curso
 foreach($cursos as $curso)
 {
-    echo "chamando um processo para {$curso}\n";
-    exec("./exemplo_curso.php $curso > /dev/null &");
-}
-
-sleep(2);
+    // abre o arquivo que armazenará os dados
+    $arquivo = fopen($argv[1] . "_{$curso}", "w");
     
-while(1)
-{
-    if( ! file_exists("./tmp/MATEMATICA_proc"))
+    // pega o total de paginas do curso
+    $total_paginas = exec("pega_num_paginas.php $curso");
+    
+    // para cada pagina
+    for($pagina = 1; $pagina <= $total_paginas; $pagina++)
     {
-        inicializa_diretorio("tmp/$curso/pgs_rodando");
-        //exec("./exemplo_conteudo.php $curso > /dev/null &");
-        break;
+        // pega o conteudo de uma pagina
+        $conteudo = shell_exec("pega_pagina.php {$curso} {$pagina}");
+        
+        // pega as urls
+        $descritor = array(
+            array("pipe", "r"),
+            array("pipe", "w"),
+            array("pipe", "w")
+        );
+        
+        $processo = proc_open("pega_urls.php", $descritor, $pipes);
+        if (is_resource($processo))
+        {
+            fwrite($pipes[0], $conteudo);
+            fclose($pipes[0]);
+            
+            $urls = stream_get_contents($pipes[1]);
+            fclose($pipes[1]);
+            fclose($pipes[2]);
+            
+            proc_close($processo);
+            
+            $urls = preg_split("/\s+/", $urls);
+            
+            // para cada url
+            for($url = 0; $url < count($urls) - 1; $url++)
+            {
+                // pega uma instituicao
+                $instituicao = shell_exec('pega_instituicao.php ' . $urls[$url]);
+                echo $instituicao;
+                
+                // pega os dados tabulados de uma instituicao
+                $processo = proc_open("extrai_instituicao.php", $descritor, $pipes);
+                if (is_resource($processo))
+                {
+                    fwrite($pipes[0], $instituicao);
+                    fclose($pipes[0]);
+                    
+                    $tabulado = stream_get_contents($pipes[1]);
+                    fclose($pipes[1]);
+                    fclose($pipes[2]);
+                    
+                    proc_close($processo);
+                    
+                    // log
+                    //echo "{$curso} {$pagina} {$url}\n";
+                    
+                    // escreve no arquivo
+                    fwrite($arquivo, $pagina ."\t". $url ."\t". $tabulado);
+                    
+                    sleep(5);
+                }
+            }
+        }
     }
+    
+    fclose($arquivo);
 }
 
